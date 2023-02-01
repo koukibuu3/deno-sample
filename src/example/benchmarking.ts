3b6aab8b17f8ed2d3984501b36d36d9dabc2e0f5
koukibuu3 <koukibuu3@gmail.com>
2023-02-01 21:36:22 +0900

    feat: ベンチマークの実施

---
+++ src/example/benchmarking.ts
@@ -0,0 +1,33 @@
+// deno bench https://examples.deno.land/benchmarking.ts
+
+Deno.bench("URL parsing", () => {
+  new URL("https://deno.land")
+})
+
+Deno.bench("Async method", async () => {
+  await crypto.subtle.digest("SHA-256", new Uint8Array([1, 2, 3]))
+})
+
+Deno.bench({
+  name: "Long form",
+  fn: () => {
+    new URL("https://deno.land")
+  },
+})
+
+Deno.bench({
+  name: "Date now()",
+  group: "timing",
+  baseline: true,
+  fn: () => {
+    Date.now()
+  },
+})
+
+Deno.bench({
+  name: "Performance.now()",
+  group: "timing",
+  fn: () => {
+    performance.now()
+  }
+})
