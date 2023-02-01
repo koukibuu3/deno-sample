a74cb8541eb6ab6f84de4f37b5383f78169eb997
koukibuu3 <koukibuu3@gmail.com>
2023-02-01 21:43:09 +0900

    feat: JSONファイルのインポート

---
+++ src/example/importing_json/main.ts
@@ -0,0 +1,7 @@
+import file from "./version.json" assert { type: "json" }
+console.log(file.version)
+
+const module = await import("./version.json", {
+  assert: { type: "json" },
+})
+console.log(module.default.version)
